870d1520fcd28ef6f3912c1d84d08c7dcab30d81
Zep Heijmans <heijmanszep@gmail.com>
2023-06-21 23:52:12 +0200

    Initial commit

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { Test } from '@/common/lib/test';
+
+@Injectable()
+export class UsersService {
+  private readonly test: Test;
+  
+  constructor() {
+    this.test = new Test();
+  }
+  
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
